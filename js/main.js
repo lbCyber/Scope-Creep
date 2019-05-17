9a761b98f8cf85f01cdcd7df14b01e6d77bb8aef
lbCyber <paul@lintbox.com>
2019-05-17 16:29:50 -0400

    Successfully calling object properties

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 const storyMain = {
   position: {
-    chapter: 1,
-    branch: 0,
-    part: 1,
-    text: 0
+    chapter: "chap1",
+    branch: "branch0",
+    part: "part1",
+    text: "",
+    background: "",
+    choice0: "",
+    choice0desc: "",
+    choice1: "",
+    choice1desc: ""
   },
   name: "",
   choices: {},
@@ -11,16 +16,30 @@ const storyMain = {
 }
 
 storyMain.grabStory = function () {
-  console.log('blep')
   $.ajax({
-    url:'./js/story.json',
+    url: './js/story.json',
     method: `GET`,
     dataType: `json`
-  }).then(function(storyData) {
-    console.log(storyData);
-    console.log('something happening')
+  }).then(function (storyData) {
+    storyMain["storyData"] = storyData;
   });
 }
 
+const readChanges = function (choice) {
+  currentChapter = storyMain.position.chapter;
+  currentBranch = storyMain.position.branch;
+  currentPart = storyMain.position.part;
+  changes = storyMain.storyData[currentChapter][currentBranch][currentPart][choice][2];
+  return changes;
+}
 
+$(function () {
+  storyMain.grabStory();
+  $('.option0').on('click', function () {
+    console.log(readChanges("choice0"));
+  });
+  $('.option1').on('click', function () {
+    console.log(readChanges("choice1"));
+  });
+});
 // {chap#}.{branch#}.{part#}.{text[0-1], background, choice[0:choicetext,1:description,2:[0:action1,1:action2]}
