1c9fee445185ac6a8f6bc0716024c0cc47ea3549
lbCyber <paul@lintbox.com>
2019-05-17 11:56:20 -0400

    Basic script framework checked, styles started

--- js/main.js
+++ js/main.js
@@ -1,12 +1,25 @@
-$(function () {
-  const storyMain = {
-    position: {
-      chapter: 1,
-      part: 1,
-      branch: 0
-    },
-    name: "",
-    choices: {},
-    state: 0
-  }
-});
+const storyMain = {
+  position: {
+    chapter: 1,
+    branch: 0,
+    part: 1,
+    text: 0
+  },
+  name: "",
+  choices: {},
+  state: 0
+}
+
+storyMain.grabStory = function () {
+  console.log('blep')
+  $.ajax({
+    url:'./js/story.json',
+    method: `GET`,
+    dataType: `json`
+  }).then(function(storyData) {
+    console.log(storyData);
+    console.log('something happening')
+  });
+}
+
+// {chap#}.{branch#}.{part#}.{text[0-1], background, choice[0:choicetext,1:description,2:[0:action1,1:action2]}
