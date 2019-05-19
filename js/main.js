54958d603974bd06f2b2e786f8dc8697dfe02c8c
lbCyber <paul@lintbox.com>
2019-05-19 00:12:00 -0400

    Added first part of story, narrative framework functional

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
+let currentChapter = "";
+let currentBranch = "";
+let currentPart = "";
+
+const base = function () {
+  return storyMain.storyData[currentChapter][currentBranch][currentPart];
+};
+
 const storyMain = {
   position: {
-    chapter: "chap1",
-    branch: "branch0",
-    part: "part1",
+    chapter: "start",
+    branch: "start",
+    part: "start",
     text: "",
     background: "",
     choice0: "",
@@ -25,21 +33,69 @@ storyMain.grabStory = function () {
   });
 }
 
-const readChanges = function (choice) {
-  currentChapter = storyMain.position.chapter;
-  currentBranch = storyMain.position.branch;
-  currentPart = storyMain.position.part;
-  changes = storyMain.storyData[currentChapter][currentBranch][currentPart][choice][2];
-  return changes;
-}
-
 $(function () {
   storyMain.grabStory();
   $('.option0').on('click', function () {
-    console.log(readChanges("choice0"));
+    $.extend(storyMain.position, readChanges("choice0"));
+    injectChanges();
   });
+
   $('.option1').on('click', function () {
-    console.log(readChanges("choice1"));
+    $.extend(storyMain.position, readChanges("choice1"));
+    injectChanges();
   });
 });
-// {chap#}.{branch#}.{part#}.{text[0-1], background, choice[0:choicetext,1:description,2:[0:action1,1:action2]}
+
+const refreshPosition = function () {
+  currentChapter = storyMain.position.chapter;
+  currentBranch = storyMain.position.branch;
+  currentPart = storyMain.position.part;
+}
+
+const readChanges = function (choice) {
+  refreshPosition();
+  changes = base()[choice][2];
+  return changes;
+}
+
+const readNewPosition = function () {
+  refreshPosition();
+  readArray = {};
+  readArray["choice0"] = base().choice0[0];
+  readArray["choice0desc"] = base().choice0[1];
+  if (base().choice1 !== undefined) {
+    readArray["choice1"] = base().choice1[0];
+    readArray["choice1desc"] = base().choice1[1];
+  } else {
+    readArray["choice1"] = false;
+    readArray["choice1desc"] = false;
+  }
+  readArray["background"] = base()["background"];
+  readArray["text"] = base()["text"];
+  return readArray;
+}
+
+const initChanges = function () {
+  $.extend(storyMain.position, readNewPosition());
+}
+
+const injectChanges = function (choice) {
+  initChanges();
+  $('.story-body-text').html(storyMain.position.text);
+  $('.option0').text(storyMain.position.choice0);
+  if (storyMain.position.choice1) {
+    if ($('.option1').hasClass('hideOption')) {
+      $('.option1').removeClass('hideOption');
+    }
+    $('.option1').text(storyMain.position.choice1);
+  } else {
+    if (!($('.option1').hasClass('hideOption'))) {
+      $('.option1').addClass('hideOption');
+    }
+  }
+  if (storyMain.position.background) {
+    $('.story-body').style.background = `url: "./assets/${storyMain.position.background}"`;
+  }
+  $('.story-body').animate({ scrollTop: 0 }, "fast");
+}
+// {chap#}.{branch#}.{part#}.text, background, choice[0:choicetext,1:description,2:{actions}}
